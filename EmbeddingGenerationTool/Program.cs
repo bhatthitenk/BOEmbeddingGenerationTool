75d0ac4aecddfcdd79103d1cb88f240a115c321c
Bhatt, Hiten <Hiten.Bhatt@c.hasbro.com>
2024-10-14 13:07:59 +0530

    Initial Project Checkin

---
+++ EmbeddingGenerationTool/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using BOEmbeddingService.Interfaces;
+using BOEmbeddingService.Services;
+
+var serviceprovider = new ServiceCollection()
+    // Add Service
+    .AddScoped<IEmbeddingService, EmbeddingService>()
+    .BuildServiceProvider();
+
+var embeddingService = serviceprovider.GetRequiredService<IEmbeddingService>();
+
+embeddingService.GetCompressMethods();
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
---
+++ EmbeddingService/Services/EmbeddingService.cs
@@ -0,0 +1,18 @@
+﻿using BOEmbeddingService.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOEmbeddingService.Services
+{
+    public class EmbeddingService : IEmbeddingService
+    {
+        public EmbeddingService() { }
+        public void GetCompressMethods()
+        {
+
+        }
+    }
+}
