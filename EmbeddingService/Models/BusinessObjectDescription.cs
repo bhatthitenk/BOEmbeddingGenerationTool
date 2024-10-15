4982d92f9dfe6ee8f384239a22bbedc6c50cd9a3
Bhatt, Hiten <Hiten.Bhatt@c.hasbro.com>
2024-10-15 15:09:28 +0530

    Change business object description model

--- EmbeddingService/Models/BusinessObjectDescription.cs
+++ EmbeddingService/Models/BusinessObjectDescription.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace BOEmbeddingService.Models
 {
-    internal class BusinessObjectDescription
+    public class BusinessObjectDescription
     {
+        [JsonPropertyName("summary")]
+        public string Summary { get; set; }
+        [JsonPropertyName("name")]
         public string Name { get; set; }
+        [JsonPropertyName("description")]
         public string Description { get; set; }
+
     }
+
 }
